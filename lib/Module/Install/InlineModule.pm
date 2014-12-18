b2436b95c07fbb6b9ca7d157ff0dff06ce59b36f
Ingy döt Net <ingy@ingy.net>
2014-12-18 11:20:09 -0800

    CPAN Release 0.02
    - Refactor to Inline-Module-0.25

--- lib/Module/Install/InlineModule.pm
+++ lib/Module/Install/InlineModule.pm
@@ -1,23 +1,21 @@
 use strict; use warnings;
 package Module::Install::InlineModule;
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 use base 'Module::Install::Base';
-use Inline::Module;
-
-# use XXX;
+use Inline::Module();
 
 sub inline {
     my ($self, %args) = @_;
 
+    my $meta = \%args;
     my $makemaker = {};
     my $postamble = Inline::Module::postamble(
         $makemaker,
-        inline => \%args,
+        inline => $meta,
     );
     $self->postamble($postamble);
-    my $IM = $Inline::Module::Self;
-    for my $module ($IM->included_modules) {
+    for my $module (Inline::Module->included_modules($meta)) {
         $self->include($module);
     }
 }
